5347c1023cb7ba5c0f0c691c2f7c775bf86f8425
AutoAFK <ranhaim85@gmail.com>
2024-10-28 16:11:42 +0200

    Working product
    I forgot to add and commit throught the work.
    BUT this is the working product.
    From now and on all the commit will be changes
    to make it more "React" with components and
    different files rather than having it all in
    a single file.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,154 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [newTask, setNewTask] = useState("");
+	const [taskList, setTaskList] = useState(
+		localStorage.getItem("task_list")
+			? JSON.parse(localStorage.getItem("task_list"))
+			: {},
+	);
+	const [trigger, setTrigger] = useState(false);
+	const ref = useRef(null);
+
+	function onChangeNewTask(event) {
+		setNewTask(event.target.value);
+	}
+
+	function onSetTrigger() {
+		setTrigger(!trigger);
+	}
+
+	function onSetTaskDone(uuid, isDone) {
+		setTaskList((prevTaskList) => {
+			const updatedTaskList = { ...prevTaskList };
+			updatedTaskList[uuid].isDone = isDone;
+			localStorage.setItem("task_list", JSON.stringify(updatedTaskList));
+			return updatedTaskList;
+		});
+	}
+
+	function submitNewTask(event) {
+		event.preventDefault();
+		setTaskList((prevTaskList) => {
+			const updatedTaskList = {
+				...prevTaskList,
+				[crypto.randomUUID()]: { task: newTask, isDone: false },
+			};
+			localStorage.setItem("task_list", JSON.stringify(updatedTaskList));
+			return updatedTaskList;
+		});
+
+		setNewTask("");
+		ref.current.focus();
+	}
+
+	function removeTask(uuid) {
+		setTaskList((prevTaskList) => {
+			const updatedTaskList = { ...prevTaskList };
+			delete updatedTaskList[uuid];
+			localStorage.setItem("task_list", JSON.stringify(updatedTaskList));
+			return updatedTaskList;
+		});
+	}
+
+	return (
+		<>
+			<div className={`text-center mx-10 ${trigger ? "opacity-50" : ""}`}>
+				<h1 className="text-4xl font-bold mb-2">Todo list</h1>
+				<button
+					type="button"
+					className="border border-gray-400 rounded-lg px-2 text-lg"
+					onClick={onSetTrigger}
+				>
+					Add Task
+				</button>
+
+				<hr className="my-4" />
+				<ul>
+					{Object.entries(taskList).map(([k, v]) => {
+						console.log(v);
+						return (
+							<li
+								key={k}
+								className="text-left border border-gray-400 my-2 rounded-lg py-2 px-4 flex justify-between items-center "
+							>
+								<div>
+									<input
+										type="checkbox"
+										checked={v.isDone}
+										onChange={(e) => onSetTaskDone(k, e.target.checked)}
+										className="mr-2"
+									/>
+									<span className="text-lg">{v.task}</span>
+								</div>
+								<button
+									type="button"
+									className="border border-black rounded-lg px-2 py-1"
+									onClick={() => removeTask(k)}
+								>
+									Remove
+								</button>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<Popup
+				trigger={trigger}
+				setTrigger={onSetTrigger}
+				submitNewTask={submitNewTask}
+				newTask={newTask}
+				onChangeNewTask={onChangeNewTask}
+				inputRef={ref}
+			/>
+		</>
+	);
+}
+
+function Popup({
+	trigger,
+	setTrigger,
+	submitNewTask,
+	newTask,
+	onChangeNewTask,
+	inputRef,
+}) {
+	if (!trigger) return null;
+	return (
+		<div className="fixed top-0 left-0 w-full h-full flex justify-center items-center">
+			<div className="p-10 border border-black rounded-lg flex items-center text-lg">
+				<form
+					onSubmit={submitNewTask}
+					className="flex flex-col items-center mb-5"
+				>
+					<label htmlFor="new_task" className="mb-1 self-start">
+						Add Task
+					</label>
+					<input
+						type="text"
+						value={newTask}
+						onChange={onChangeNewTask}
+						ref={inputRef}
+						className="border border-gray-400 rounded-lg mb-5"
+					/>
+
+					<div>
+						<input
+							type="submit"
+							className="border rounded-lg bg-slate200 px-2"
+						/>
+						<button
+							type="button"
+							className="border rounded-lg bg-slate200 px-2 ml-2"
+							onClick={setTrigger}
+						>
+							Close
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
